41bcf225d25143523c2f29831b5ac865491f2e7f
Simon Coulter <simnon13@gmail.com>
2023-04-30 08:34:46 +0100

    Add Google Analytics tracking Id

--- gatsby-config.js
+++ gatsby-config.js
@@ -107,12 +107,12 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: `ADD YOUR TRACKING ID HERE`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `G-V2JCRMKRVH`,
+      },
+    },
     {
       resolve: `gatsby-plugin-feed`,
       options: {
